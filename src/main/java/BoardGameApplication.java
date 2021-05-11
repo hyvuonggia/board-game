0da59f959a7f85b612c52f161fbf808993ea6736
Vuong Gia Hy <hyvuonggia@gmail.com>
2021-05-11 14:13:44 +0200

    Added pom.xml and checkstyle.xml

---
+++ src/main/java/BoardGameApplication.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BoardGameApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Board Game");
+        stage.show();
+    }
+}
