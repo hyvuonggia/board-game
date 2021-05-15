5d73455db3dd8b6499a6debb15bdabcd58da6be7
Vuong Gia Hy <hyvuonggia@gmail.com>
2021-05-15 13:29:34 +0200

    Added methods into the model and ChessPiece class

--- src/main/java/boardgame/model/BoardGameModel.java
+++ src/main/java/boardgame/model/BoardGameModel.java
@@ -1,4 +1,47 @@
 package boardgame.model;
 
 public class BoardGameModel {
+    public static final int BOARD_SIZE = 5;
+
+    public ChessPiece[] pieces;
+
+    public BoardGameModel() {
+        this(new ChessPiece(ChessColor.BLUE, new Position(0, 0)),
+                new ChessPiece(ChessColor.BLUE, new Position(0, 1)),
+                new ChessPiece(ChessColor.BLUE, new Position(0, 2)),
+                new ChessPiece(ChessColor.BLUE, new Position(0, 3)),
+                new ChessPiece(ChessColor.BLUE, new Position(0, 4)),
+                new ChessPiece(ChessColor.BLUE, new Position(1, 0)),
+                new ChessPiece(ChessColor.BLUE, new Position(1, 4)),
+                new ChessPiece(ChessColor.RED, new Position(4, 0)),
+                new ChessPiece(ChessColor.RED, new Position(4, 1)),
+                new ChessPiece(ChessColor.RED, new Position(4, 2)),
+                new ChessPiece(ChessColor.RED, new Position(4, 3)),
+                new ChessPiece(ChessColor.RED, new Position(4, 4)),
+                new ChessPiece(ChessColor.RED, new Position(3, 0)),
+                new ChessPiece(ChessColor.RED, new Position(3, 4))
+        );
+    }
+
+    public BoardGameModel(ChessPiece... pieces) {
+        checkPieces(pieces);
+        this.pieces = pieces.clone();
+    }
+
+    public boolean isOnBoard(Position position) {
+        if (position.row() >= 0 && position.row() < BOARD_SIZE && position.col() >= 0 && position.col() < BOARD_SIZE) {
+            return true;
+        }
+        return false;
+    }
+
+    public void checkPieces(ChessPiece[] pieces){
+        for (ChessPiece piece : pieces) {
+            if (!isOnBoard(piece.getPosition())) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+
 }
--- src/main/java/boardgame/model/ChessPiece.java
+++ src/main/java/boardgame/model/ChessPiece.java
@@ -2,16 +2,31 @@ package boardgame.model;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChessPiece {
 
     private ChessColor chessColor;
-    private final ObjectProperty<Position> position = new SimpleObjectProperty<>();
+    private ObjectProperty<Position> position = new SimpleObjectProperty<>();
 
+    public ChessColor getChessColor() {
+        return chessColor;
+    }
+
+    public Position getPosition() {
+        return position.get();
+    }
+
+    public ObjectProperty<Position> positionProperty() {
+        return position;
+    }
+
+    public ChessPiece(ChessColor chessColor, Position position) {
+        this.chessColor = chessColor;
+        this.position.set(position);
+    }
+
+    public void moveTo(Direction direction){
+        Position newPosition = this.position.get().moveTo(direction);
+        this.position.set(newPosition);
+    }
 }
