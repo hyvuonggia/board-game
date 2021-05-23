b35a318e10c843a4769ccbf36d2b96879ea1ec63
Vuong Gia Hy <hyvuonggia@gmail.com>
2021-05-23 12:07:54 +0200

    Added restart button

--- src/main/java/boardgame/controller/ScoreController.java
+++ src/main/java/boardgame/controller/ScoreController.java
@@ -6,11 +6,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import org.tinylog.Logger;
 
+import java.io.IOException;
 import java.util.List;
 
 public class ScoreController {
@@ -44,5 +50,14 @@ public class ScoreController {
     private void handleExitButton(ActionEvent event){
         Logger.info("Clicked on Exit button");
         System.exit(0);
+    }
+
+    @FXML
+    private void handleRestartButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/nameui.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
